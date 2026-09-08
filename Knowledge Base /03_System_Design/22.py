9c23b16020f9536b9a09e989d24f09151b0b66cc
shivanath samudrala <samudralashivanath6@gmail.com>
2026-09-08 11:23:13 +0530

    Add script to sum two numbers and print result

---
+++ Knowledge Base /03_System_Design/22.py
@@ -0,0 +1,9 @@
+# Storing numbers in variables
+num1 = 15
+num2 = 25
+
+# Adding the numbers
+result = num1 + num2
+
+# Printing the result using an f-string
+print(f"The sum of {num1} and {num2} is {result}")
